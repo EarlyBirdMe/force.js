6be9596d070432ff5ac4c72790fe93a9f1be4ee8
Leo Deng <myst.dg@gmail.com>
2013-05-27 13:19:17 +0800

    update demo

--- demo.js
+++ demo.js
@@ -1,11 +1,11 @@
 require(['forces/dom', 'forces/string', 'front.js/front.min'], function(dom, string, front) {
-  var data = [{letter:'R'},{letter:'A'},{letter:'I'},{letter:'N'},{letter:'B'},{letter:'O'},{letter:'W'}];
+  var data = [{letter: 'R', bg: '#f00'}, {letter: 'A', bg: '#f80'}, {letter: 'I', bg: '#ff0'}, {letter: 'N', bg: '#62d815'}, {letter: 'B', bg: '#00eaf8'}, {letter: 'O', bg: '#000490'}, {letter: 'W', bg: '#f100c3'}];
   var node = dom.byId('node');
-  var tmpl = '<div class="letter">{{letter}}</div>';
+  var tmpl = '<div class="cell" style="background:{{bg}}">{{letter}}</div>';
   front.parse(data, node, tmpl);
   dom.addClass(node, 'rainbow');
 
-  var cells = dom.byClass('letter', node);
+  var cells = dom.byClass('cell', node);
   var titleString = 'force.js / ';
   for(var i = 0; i < cells.length; i++) {
     titleString = string.concat(titleString, string.lower(cells[i].innerHTML));
